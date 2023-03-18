7fc29fe26782f945ecbe690080f62ecca2d52475
asm-shaikat <asmshaikat1952@gmail.com>
2023-03-18 22:17:42 +0600

    refactor: shift cipher

--- ShiftCipher.py
+++ ShiftCipher.py
@@ -1,34 +1,29 @@
-def Encryption(message):
-    SendMessage = ''
-    for i in message:
-        if(ord(i)>=65 and ord(i)<=90):
-            a = ord(i) + 1
-            if(a>90):
-                a = a % 90 + 64
-            SendMessage = SendMessage + chr(a)
-        elif(ord(i)>=97 and ord(i)<=122):
-            a = ord(i) + 1
-            if(a > 122):
-                a = a % 122 + 96
-            SendMessage = SendMessage + chr(a)
+def Encryption(text):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + 1 - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) + 1 - 97) % 26 + 97)
         else:
-            send =  send + chr(i) + 1
-    print("Decrypted Message: " + SendMessage)
-    Decryption(SendMessage)   
-
-
-def Decryption(SendMessage):
-    GetMessage = ''
-    for i in SendMessage:
-        if(ord(i)>=65 and ord(i)<=90):
-            GetMessage = GetMessage + chr((ord(i) - 1 -65) % 26 + 65)
-        elif(ord(i)>=97 and ord(i)<=122):
-            GetMessage = GetMessage + chr((ord(i) - 1 - 97) % 26 + 97)
+            result += char
+    print("Encrypted Ciphertext: "+ result)
+    Decryption(result)
+    
+def Decryption(ciphertext):
+    result = ""
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        if char.isupper():
+            result += chr((ord(char) - 1 - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) - 1 - 97) % 26 + 97)
         else:
-            GetMessage = GetMessage + chr(ord(i) - 1)
-    print("Message will get: " + GetMessage)
-        
+            result += char
+    print("Decrypted Message: " + result)
 
-message = input("Enter the message: ")
 
-Encryption(message)
+plaintext = input("Enter the plaintext: ")
+Encryption(plaintext)
+
